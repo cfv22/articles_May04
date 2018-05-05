aa6711c24ca3ee3d19f3b88c92270fd212c7385d
CFV <cristian.fraile@morningstar.com>
2018-05-04 19:09:32 -0500

    added devise

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,31 @@
+class ArticlesController < ApplicationController
+    # before_action :authenticate_user!
+    def index
+        @articles = Article.all 
+    end
+    def new
+        @article = Article.new
+    end
+    
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            redirect_to articles_path
+        else 
+            render :new
+        end 
+         
+    end
+    def destroy
+    article = Article.find(params[:id])
+    article.destroy
+    redirect_to articles_path
+  end
+
+    private 
+    def article_params
+        params.require(:article).permit(:title, :body, :published)
+    end
+    
+    
+end
---
+++ test/models/article_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class ArticleTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "article is not created without a title" do 
+    article = Article.new
+    assert_not article.save, "article should not be created without a title"
+  end
+  
+  test ".word_count returns the correct number of words" do
+    article = Article.new(title: "Art 1", body: "Hola mundo, esto es una prueba!")
+    assert_equal 6, article.word_count
+  end
+  
+  test "published scope only returns published articles" do
+    articles = Article.published
+    assert_equal 1, articles.length
+  end
+end
